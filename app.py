a376fdebcd6104fc252dd1ed54826c5ff8cb9978
a24ibrah <a24ibrah@users.noreply.github.com>
2022-06-19 01:32:24 -0400

    Modify app file

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 def hello():
     return '<h1>Hello from a Flask app</h2>'
 
+# greeting message
+@app.route('/<username>')
+def show_user(username):
+    # Greet the user
+    return f'Hello {username} !'
 
 if __name__ == "__main__":
     app.run(debug=True)
